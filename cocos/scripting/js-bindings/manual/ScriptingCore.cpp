fba98bca7342f2410a7927019123eeb12bf9b3c0
Vyshnav <vyshnav.s.deepak@gmail.com>
2022-10-06 16:06:58 +0530

    Added evalStringInCocosThread method
    Wraps evalString call inside scheduler's perfomFunctionInCocosThread

--- cocos/scripting/js-bindings/manual/ScriptingCore.cpp
+++ cocos/scripting/js-bindings/manual/ScriptingCore.cpp
@@ -559,6 +559,14 @@ bool ScriptingCore::evalString(const char *string)
     return evalString(string, &retVal);
 }
 
+void ScriptingCore::evalStringInCocosThread(const std::string string)
+{
+    auto scheduler = cocos2d::Director::getInstance()->getScheduler();
+    scheduler->performFunctionInCocosThread([this, string](){
+        evalString(string.c_str());
+    });
+}
+
 void ScriptingCore::start()
 {
     _engineStartTime = std::chrono::steady_clock::now();
